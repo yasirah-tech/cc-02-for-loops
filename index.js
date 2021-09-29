9e577c1606860966b276eb262bc0f3dc7dd45675
Yasirah <yasirahboyce1@gmail.com>
2021-09-29 14:44:06 +0000

    code challenge 02 for loops

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-//1
-function countToTen(){
+//1 RAN
+//function countToTen(){ 
+ for(var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//2 RAN 
+function countFromOne(num){
+const array = [];
+  for(let i = 1; i < num; i++) {
+    array.push(i);
+  }
+ return array 
+ }
+
+//3 RAN
+//function countEveryEven(){
+    for (var i=0; i<=15; i++) {
+        if ( i === 0 || i % 2 === 0)
+        console.log(i);
   
 }
 
-//2
-function countFromOne(){
-  
-}
 
-//3
-function countEveryEven(){
-  
-}
+//4 RAN
+//function countEveryOdd(){
+    for (var i=0; i<=15; i++) {
+        if ( i % 2 === 1)
+        console.log(i);
+  }
 
-//4
-function countEveryOdd(){
-  
-}
+//5
+// function countEvens(){
+    function countEvens(){
+    for (var i=0; i<=15; i++) {
+        if ( i === 1 || i % 2 === 0)
+        
+        console.log(i);}
 
-//5 
-function countEvens(){
-  
-}
-
+    }
+console.log(countFromOne(13))
